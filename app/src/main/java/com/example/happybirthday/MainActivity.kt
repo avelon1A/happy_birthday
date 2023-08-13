0e9661e646706d3bde13180c6ae8c27b0d29ec89
avelon1A <amanavelon@gmail.com>
2023-08-13 11:31:54 +0530

    addd clicklistner, when button is clicked then it will  show a toast

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.happybirthday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameInput: EditText = findViewById(R.id.nameInput)
+        val showToastButton: Button = findViewById(R.id.buttonok)
+
+        showToastButton.setOnClickListener {
+            val name = nameInput.text.toString()
+            Toast.makeText(this, "button clicked and your name is $name", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-}
